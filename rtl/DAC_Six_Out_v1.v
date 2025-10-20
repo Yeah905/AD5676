298b33c3275bea0c2aa9aa6b165fa5649df0213c
Yeah905 <130521918+Yeah905@users.noreply.github.com>
2025-10-20 22:57:45 +0800

    Refactor DAC module to use 25MHz clock

--- rtl/DAC_Six_Out_v1.v
+++ rtl/DAC_Six_Out_v1.v
@@ -9,18 +9,20 @@ module DAC_Six_Out_v1(
     input                 key_wave    ,  //电压输出控制按键
     //DA芯片接口
     output                da_clk      ,  //DAC驱动时钟
-    output    [23:0]      da_data        //输出给DA的数据 
+	output				  ldac_n	  ,
+	output				  sync_n	  ,
+    output                sdin        //输出给DA的数据 
 );
 
 //parameter define
-parameter  CNT_MAX = 22'd400_0000;      //50MHz时钟下计数20ms
+parameter  CNT_MAX = 22'd100_0000;      //50MHz时钟下计数20ms
 
 //wire define 
 wire             rst_n          ;  // 复位，低有效
 wire             locked         ;  //PLL时钟锁定信号
-wire             clk_50m       ;  //50MHz时钟
-wire    [8:0]    rd_addr        ;  //ROM读地址
-wire    [23:0]    rd_data        ;  //ROM读出的数据
+wire             clk_25m       ;  //50MHz时钟
+wire    [2:0]    rd_addr        ;  //ROM读地址
+wire    [23:0]    rom_data        ;  //ROM读出的数据
 wire             key_wave_filter;  //波形控制按键消抖后的按键值
 
 //通过系统复位信号和PLL时钟锁定信号来产生一个新的复位信号
@@ -30,22 +32,22 @@ assign   rst_n = sys_rst_n & locked;
 pll	u_pll (
     .areset             ( ~sys_rst_n ),
     .inclk0             ( sys_clk ),
-    .c0                 ( clk_50m ),
+    .c0                 ( clk_25m ),
     .locked             ( locked )
     );
 
 //ROM存储波形
 rom	u_rom (
     .address            ( rd_addr ),
-    .clock              ( clk_50m ),
-    .q                  ( rd_data )
+    .clock              ( clk_25m ),
+    .q                  ( rom_data )
     );
 
 //例化按键消抖模块
 key_debounce #(
     .CNT_MAX            (CNT_MAX        ))
 u_key_wave_debounce(
-    .sys_clk            (clk_50m       ),
+    .sys_clk            (clk_25m        ),
     .sys_rst_n          (rst_n          ),
     .key                (key_wave       ),
     .key_filter         (key_wave_filter)
@@ -53,13 +55,15 @@ u_key_wave_debounce(
 
 //DA数据发送
 da_wave_send u_da_wave_send(
-    .clk                (clk_50m       ),
+    .clk                (clk_25m        ),
     .rst_n              (rst_n          ),
     .key_wave_filter    (key_wave_filter),
-    .rd_data            (rd_data        ),
-    .rd_addr            (rd_addr        ),
-    .da_clk             (da_clk         ),
-    .da_data            (da_data        )
+    .rom_data           (rom_data       ),
+    .rom_addr           (rd_addr        ),
+    .sclk               (da_clk         ),
+	.sync_n				(sync_n			),
+	.ldac_n				(ldac_n			),
+    .sdin               (sdin           )
     );
 	
 endmodule
